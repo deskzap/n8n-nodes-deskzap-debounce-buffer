606cd5b79f07ee97ed5d46a674102bf46f735739
deskzap <deskzap@users.noreply.github.com>
2025-11-28 14:02:17 -0300

    chore: add index.ts exports and update main (v0.3.10)

---
+++ index.ts
@@ -0,0 +1,3 @@
+export { DeskzapDebounceBuffer } from './nodes/DeskzapDebounceBuffer/DeskzapDebounceBuffer.node';
+export { SmartBuffer } from './nodes/SmartBuffer/SmartBuffer.node';
+export { RedisCredentials } from './credentials/RedisCredentials.credentials';
