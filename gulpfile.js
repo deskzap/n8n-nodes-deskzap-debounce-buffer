36469345567a5d14428d8635084725f8c687546e
deskzap <deskzap@users.noreply.github.com>
2025-11-28 13:44:22 -0300

    chore: fix build config (v0.3.8)

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 const { src, dest } = require('gulp');
 
 function copyIcons() {
-	return src('nodes/**/*.{png,svg}')
-		.pipe(dest('dist/nodes'));
+	return src(['nodes/**/*.{png,svg}', 'credentials/**/*.{png,svg}'], { base: '.' })
+		.pipe(dest('dist'));
 }
 
 exports['build:icons'] = copyIcons;
